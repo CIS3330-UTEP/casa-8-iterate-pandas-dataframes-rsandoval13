cbe69cae3b8edcc88916a23773af2c36a5094fec
rsandoval13 <rsandoval13@miners.utep.edu>
2024-02-13 23:29:09 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,8 @@
+import pandas as pd
+df= pd.read_csv('big-mac-full-index.csv')
 
+
+for x,r in df.iterrows():
+    print(r['name'],r['local_price'],r['currency_code'])
+
+print(df.apply(lambda row: row['name']+" "+ str(row['local_price']) +" "+ str(row['currency_code']), axis=1))
